30382fbae2b9fc0eb93ce8834d7829b93ff9bf3c
Hamza123Hussain <hamzahussain14.hh@gmail.com>
2024-04-22 04:21:18 +0500

    loading added

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import { useState, useEffect } from 'react'
 export default function Trending() {
   const [movieData, setMovieData] = useState([])
   const { page, setpage } = Usepagecontext()
+  const [loading, setloading] = useState(true)
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -14,18 +15,76 @@ export default function Trending() {
           `https://api.themoviedb.org/3/discover/movie?api_key=2422277ae0eeb10b737f5e464ad3e1c3&page=${page}`
         )
         if (res.ok) {
+          setloading(false)
           const data = await res.json()
           setMovieData(data.results)
         } else {
           throw new Error('Failed to fetch data')
         }
       } catch (error) {
+        setloading(false)
         console.error(error)
       }
     }
     fetchData()
   }, [page]) // Fetch data whenever page changes
 
+  if (loading) {
+    return (
+      <div className=" flex justify-center items-center gap-2 min-h-screen">
+        <div className="lds-roller">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="lds-roller">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>{' '}
+        <div className="lds-roller">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>{' '}
+        <div className="lds-roller">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>{' '}
+        <div className="lds-roller">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    )
+  }
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
